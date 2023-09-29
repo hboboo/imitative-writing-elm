10d5bae24460a333415fb4ce5de308134ab343dd
hb <515870153@qq.com>
2023-09-29 20:45:27 +0800

    updata

---
+++ src/components/common/ratingStar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="rating_container">
+    <section class="star_container">
+      <svg-icon icon-class="star" v-for="num in 5" :key="num" class="grey_fill"></svg-icon>
+    </section>
+    <div class="star_overflow" :style="'width:' + rating*2/5 + 'rem'">
+      <section class="star_container">
+        <svg-icon icon-class="star" v-for="num in 5" :key="num" class="orange_fill"></svg-icon>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import svgIcon from './svgIcon.vue'
+export default {
+  name: 'RatingStar',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+
+  components: {
+    svgIcon
+  },
+
+  props: ['rating']
+};
+</script>
+
+<style lang="less" scoped>
+  @import '../../style/mixin.less';
+
+  .rating_container{
+    position: relative;
+    top: .2rem;
+    .wh(2rem, .4rem);
+    .star_overflow{
+      overflow: hidden;
+      position: relative;
+      height: 100%;
+    }
+    .star_container{
+      position: absolute;
+      display: flex;
+      width: 2rem;
+      height: 0.4rem;
+      top: -0.02rem;
+      left: -0.02rem;
+      .grey_fill{
+        fill: #d1d1d1;
+      }
+      .orange_fill{
+        fill: #ff9a0d;
+      }
+    }
+  }
+</style>
--- src/components/common/shoplist.vue
+++ src/components/common/shoplist.vue
@@ -8,8 +8,19 @@
           </section>
           <div class="shop_right">
             <header class="shop_detail_header">
-              <h4 class="shop_title ellipsis">品牌测试</h4>
+              <h4 class="shop_title ellipsis premium">{{item.name}}</h4>
+              <ul class="shop_detail_ul">
+                <li v-for="item in item.supports" :key="item.id" class="supports">{{item.icon_name}}</li>
+              </ul>
             </header>
+            <h5 class="rating_order_num">
+              <section class="rating_order_num_left">
+                <section class="rating_section">
+                  <rating-star :rating='item.rating'></rating-star>
+                  <span class="rating_num">{{item.rating}}</span>
+                </section>
+              </section>
+            </h5>
           </div>
         </router-link>
       </li>
@@ -19,6 +30,7 @@
 
 <script>
 import axios from 'axios';
+import ratingStar from './ratingStar.vue'
 export default {
   name: 'Shoplist',
 
@@ -39,6 +51,9 @@ export default {
   methods: {
     
   },
+  components: {
+    ratingStar
+  }
 };
 </script>
 
@@ -61,7 +76,6 @@ export default {
   }
   .shop_link {
   display: flex;
-  // align-items: center;
   flex: auto;
   }
   .shop_right{
@@ -70,7 +84,45 @@ export default {
       .fj;
       align-items: center;
       .shop_title{
-        
+        width: 8.5rem;
+        color: #333;
+        padding-top: .01rem;
+        .font(0.65rem, 0.65rem, 'PingFangSC-Regular');
+      }
+      .premium::before{
+        content: '品牌';
+        display: inline-block;
+        font-size: 0.5rem;
+        line-height: .6rem;
+        color: #333;
+        background-color: #ffd930;
+        padding: 0 0.1rem;
+        border-radius: 0.1rem;
+        margin-right: 0.2rem;
+      }
+      .shop_detail_ul{
+        display: flex;
+        transform: scale(.8);
+        margin-right: -0.3rem;
+        .supports{
+          .sc(0.5rem, #999);
+          border: 0.025rem solid #f1f1f1;
+          padding: 0 0.04rem;
+          border-radius: 0.08rem;
+          margin-left: 0.05rem;
+        }
+      }
+    }
+    .rating_order_num{
+      .fj(space-between);
+      height: 0.6rem;
+      margin-top: 0.52rem;
+      .rating_order_num_left{
+        .fj(flex-start);
+        .rating_num{
+          .sc(0.4rem, #ff6000);
+          margin: 0 0.2rem;
+        }
       }
     }
   }
